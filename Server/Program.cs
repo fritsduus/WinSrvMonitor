d587e74453d30e1504a6170ecdf6984203ec4be3
Frits Duus Jørgensen <frits.duus@gmail.com>
2015-10-16 14:44:07 +0200

    Added creation of ActorSystem.

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace WinSrvMonitor.Server
 {
     class Program
     {
+        public static ActorSystem MonitorActorSystem;
+
         static void Main(string[] args)
         {
+            MonitorActorSystem = ActorSystem.Create("WinSrvMonitor");
+
+            // blocks the main thread from exiting until the actor system is shut down
+            MonitorActorSystem.AwaitTermination();
         }
     }
 }
